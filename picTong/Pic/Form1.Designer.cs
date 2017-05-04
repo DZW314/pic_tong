07b735db6735d6c17e002a131871a88e1dc8f25e
OrangeChina <276241095@163.com>
2017-05-04 18:22:26 +0800

    v1.0

--- picTong/Pic/Form1.Designer.cs
+++ picTong/Pic/Form1.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
@@ -37,23 +39,57 @@
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxWidth = new System.Windows.Forms.TextBox();
+            this.textBoHeight = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.cbSerial = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSwitch = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.btnSend);
+            this.panel1.Controls.Add(this.button7);
             this.panel1.Controls.Add(this.button3);
             this.panel1.Controls.Add(this.button5);
             this.panel1.Controls.Add(this.button4);
             this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.button1);
-            this.panel1.Location = new System.Drawing.Point(12, 249);
+            this.panel1.Location = new System.Drawing.Point(6, 262);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(517, 47);
             this.panel1.TabIndex = 1;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(306, 3);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(44, 40);
+            this.btnSend.TabIndex = 8;
+            this.btnSend.Text = "下载";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(256, 3);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(44, 40);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "输出";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button3
             // 
@@ -97,6 +133,7 @@
             this.button2.Size = new System.Drawing.Size(44, 40);
             this.button2.TabIndex = 1;
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
@@ -119,24 +156,134 @@
             this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(300, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(517, 237);
+            this.pictureBox1.Size = new System.Drawing.Size(517, 243);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "水平像素px";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "垂直像素px";
+            // 
+            // textBoxWidth
+            // 
+            this.textBoxWidth.Location = new System.Drawing.Point(81, 6);
+            this.textBoxWidth.MaxLength = 4;
+            this.textBoxWidth.Name = "textBoxWidth";
+            this.textBoxWidth.Size = new System.Drawing.Size(78, 21);
+            this.textBoxWidth.TabIndex = 4;
+            this.textBoxWidth.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.number_KeyPress);
+            // 
+            // textBoHeight
+            // 
+            this.textBoHeight.Location = new System.Drawing.Point(81, 40);
+            this.textBoHeight.MaxLength = 4;
+            this.textBoHeight.Name = "textBoHeight";
+            this.textBoHeight.Size = new System.Drawing.Size(78, 21);
+            this.textBoHeight.TabIndex = 5;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(165, 40);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "更改";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // cbSerial
+            // 
+            this.cbSerial.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbSerial.FormattingEnabled = true;
+            this.cbSerial.Location = new System.Drawing.Point(81, 77);
+            this.cbSerial.Name = "cbSerial";
+            this.cbSerial.Size = new System.Drawing.Size(78, 20);
+            this.cbSerial.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "串口：";
+            // 
+            // btnSwitch
+            // 
+            this.btnSwitch.Location = new System.Drawing.Point(165, 75);
+            this.btnSwitch.Name = "btnSwitch";
+            this.btnSwitch.Size = new System.Drawing.Size(75, 23);
+            this.btnSwitch.TabIndex = 9;
+            this.btnSwitch.Text = "打开";
+            this.btnSwitch.UseVisualStyleBackColor = true;
+            this.btnSwitch.Click += new System.EventHandler(this.btnSwitch_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.toolStripProgressBar1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 312);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.toolStrip1.Size = new System.Drawing.Size(829, 25);
+            this.toolStrip1.TabIndex = 10;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(59, 22);
+            this.toolStripLabel1.Text = "下载进度";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 22);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(829, 308);
+            this.ClientSize = new System.Drawing.Size(829, 337);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.btnSwitch);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbSerial);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.textBoHeight);
+            this.Controls.Add(this.textBoxWidth);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "看图工具";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -151,7 +298,19 @@
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
         private System.Windows.Forms.Button button3;
-
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxWidth;
+        private System.Windows.Forms.TextBox textBoHeight;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.ComboBox cbSerial;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSwitch;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
     }
 }
 
--- picTong/Pic/Form1.cs
+++ picTong/Pic/Form1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.IO.Ports;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,11 +9,15 @@ using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using INIFILE;
+using System.Text.RegularExpressions;
 
 namespace Pic
 {
     public partial class Form1 : Form
     {
+        SerialPort sp1 = new SerialPort();
+        //sp1.ReceivedBytesThreshold = 1;//只要有1个字符送达端口时便触发DataReceived事件 
         public Form1()
         {
             InitializeComponent();
@@ -26,6 +32,8 @@ namespace Pic
         //打开
         private void button1_Click(object sender, EventArgs e)
         {
+            //设置文件名为空
+            openFileDialog1.FileName = "";
              //过滤条件
             openFileDialog1.Filter = "Image File(*.bmp,*.wmf,*.ico,*.jpg,*.bng)|*.bmp;*.wmf;*.ico;*.jpg;*.bng";
             //是否可多选
@@ -34,8 +42,12 @@ namespace Pic
             {
                 pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
                 fileName = openFileDialog1.FileName;
+                //调取图像的像素尺寸
+                Object height = pictureBox1.Image.Height;
+                Object width = pictureBox1.Image.Width;
+                textBoHeight.Text = height.ToString();
+                textBoxWidth.Text = width.ToString();
             }
-
         }
         //保存
         private void button2_Click(object sender, EventArgs e)
@@ -49,8 +61,9 @@ namespace Pic
                    "JPEG File(*.jpg)|*.jpg|" +
                    "PNG File(*.png)|*.png";
                 saveFileDialog1.FileName = fileName;//原文件名保存
-                saveFileDialog1.CheckFileExists = false;//存在相同文件名不提示
-                bmp.Save(saveFileDialog1.FileName, ImageFormat.Bmp);//存储图片，
+            //    saveFileDialog1.CheckFileExists = false;//存在相同文件名不提示
+                //bmp.Save(saveFileDialog1.FileName, ImageFormat.Bmp);//存储图片，
+                bmp.Save(saveFileDialog1.FileName);//存储图片，
             }
             
          }
@@ -120,5 +133,260 @@ namespace Pic
                 fileName = saveFileDialog1.FileName;
             }
         }
+        //更改尺寸信息
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //if (pictureBox1.Image != null)
+            //{
+            //    Bitmap bmp = new Bitmap(pictureBox1.Image);	//指定图像
+            //    Graphics graphics = pictureBox1.CreateGraphics(); 				//声明画板
+            //    graphics.Clear(this.BackColor);//清除当前窗体的背景色
+            //    int width = Convert.ToInt32(Convert.ToDouble(0.5 * bmp.Width));//缩放宽度
+            //    int height = Convert.ToInt32(Convert.ToDouble(0.5 * bmp.Height));//缩放高度
+            //    graphics.DrawImage(bmp, pictureBox1.Left, pictureBox1.Top, width, height);//绘制图像
+            //    image = pictureBox1.Image;
+            //}
+            //if (pictureBox1.Image != null)
+            //{
+            //    Bitmap bmp1 = new Bitmap(pictureBox1.Image);//使用图片创建Bitmap对象
+            //    bmp1.RotateFlip(RotateFlipType.RotateNoneFlipXY);//旋转
+            //    pictureBox1.Image = bmp1;
+            //    image = (Image)bmp1.Clone();
+            //}            
+        }
+
+        //只允许输入数字和退格
+        private void number_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(Char.IsNumber(e.KeyChar)) && e.KeyChar != (char)8)
+            {
+                MessageBox.Show("请输入数字！");
+                e.Handled = true;
+            }
+        }
+        //输出RGB
+        private void button7_Click(object sender, EventArgs e)
+        {
+            ////定义4个颜色结构体
+            //Color c = new Color();
+
+            //int r, g, b;
+
+            //Bitmap bmp1 = new Bitmap(pictureBox1.Image);//创建Bitmap对象
+            //for (int i = 0; i < bmp1.Width - 1; i++)
+            //{
+            //    for (int j = 0; j < bmp1.Height - 1; j++)
+            //    {
+            //        //获得像素颜色值
+            //        c = bmp1.GetPixel(i, j);
+            //        //红色分量值
+            //        r = c.R;
+            //        //绿色分量值
+            //        g = c.G;
+            //        //蓝色分量值
+            //        b = c.B;
+            //    }
+
+            //}
+            if (pictureBox1.Image != null)
+            {
+                //定义一个Bitmap对象来存放图片
+                Bitmap bmp = new Bitmap(pictureBox1.Image);
+                //Bitmap bmp =(Bitmap)Image.FromFile(fileName);
+
+                Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+                //以可读写的方式锁定全部位图像素
+                BitmapData bmpData = bmp.LockBits(rect, ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);  //bmp.PixelFormat
+
+                //得到首地址
+                IntPtr ptr = bmpData.Scan0;
+                //位图字节数
+                int bytes = bmp.Width * bmp.Height * 3;
+                //定义位图数组
+                byte[] rgbValues = new byte[bytes];
+                //复制被锁定的位图像素值到该数组内
+                System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
+                File.WriteAllBytes("./rgb", rgbValues);
+
+                //int blockLength = 4096;
+                //int totalLen = rgbValues.Length;
+
+                //byte[] fileblock = new byte[blockLength];
+
+                //int temp = totalLen / blockLength;
+                //int remainder = totalLen % blockLength;
+                //if (remainder > 0)
+                //    temp += 1;
+                //for (int i = 0; i < temp; i++)
+
+                //{
+                //    Array.Copy(rgbValues, i, fileblock, 0, rgbValues.Length - blockLength);
+
+                //}
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            INIFILE.Profile.LoadProfile();//加载所有
+
+            //检查是否含有串口
+            string[] str = SerialPort.GetPortNames();
+            if (str == null)
+            {
+                MessageBox.Show("本机没有串口！", "Error");
+                return;
+            }
+
+            //添加串口项目
+            foreach (string s in System.IO.Ports.SerialPort.GetPortNames())
+            {//获取有多少个COM口
+             //System.Diagnostics.Debug.WriteLine(s);
+                cbSerial.Items.Add(s);
+            }
+
+            //串口设置默认选择项
+            //DZW
+            cbSerial.SelectedIndex = 0;         //note：获得COM1口，但别忘修改
+
+            sp1.BaudRate = 115200;
+
+            Control.CheckForIllegalCrossThreadCalls = false;    //这个类中我们不检查跨线程的调用是否合法(因为.net 2.0以后加强了安全机制,，不允许在winform中直接跨线程访问控件的属性)
+            sp1.DataReceived += new SerialDataReceivedEventHandler(sp1_DataReceived);
+            //sp1.ReceivedBytesThreshold = 1;
+
+            //准备就绪              
+            sp1.DtrEnable = true;
+            sp1.RtsEnable = true;
+            //设置数据读取超时为1秒
+            sp1.ReadTimeout = 1000;
+
+            sp1.Close();
+
+        }
+        void sp1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (sp1.IsOpen)     //此处可能没有必要判断是否打开串口，但为了严谨性，我还是加上了
+            {             
+                byte[] byteRead = new byte[sp1.BytesToRead];    //BytesToRead:sp1接收的字符个数
+
+                //'发送16进制按钮'
+                try
+                    {
+                        Byte[] receivedData = new Byte[sp1.BytesToRead];        //创建接收字节数组
+                        sp1.Read(receivedData, 0, receivedData.Length);         //读取数据
+                        //string text = sp1.Read();   //Encoding.ASCII.GetString(receivedData);
+                        sp1.DiscardInBuffer();                                  //清空SerialPort控件的Buffer
+                        //这是用以显示字符串
+                        //    string strRcv = null;
+                        //    for (int i = 0; i < receivedData.Length; i++ )
+                        //    {
+                        //        strRcv += ((char)Convert.ToInt32(receivedData[i])) ;
+                        //    }
+                        //    txtReceive.Text += strRcv + "\r\n";             //显示信息
+                        //}
+                        string strRcv = null;
+                        //int decNum = 0;//存储十进制
+                        for (int i = 0; i < receivedData.Length; i++) //窗体显示
+                        {
+
+                            strRcv += receivedData[i].ToString("X2");  //16进制显示
+                        }
+                        
+                    }
+                    catch (System.Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "出错提示");
+                    }
+            }
+            else
+            {
+                MessageBox.Show("请打开某个串口", "错误提示");
+            }
+        }
+
+        private void btnSwitch_Click(object sender, EventArgs e)
+        {
+            //serialPort1.IsOpen
+            if (!sp1.IsOpen)
+            {
+                try
+                {
+                    //设置串口号
+                    string serialName = cbSerial.SelectedItem.ToString();
+                    sp1.PortName = serialName;
+
+                    //设置各“串口设置”
+                    Int32 iBaudRate = 115200;
+                    Int32 iDateBits = 8;
+                    string strStopBits = "1";
+
+                    sp1.BaudRate = iBaudRate;       //波特率
+                    sp1.DataBits = iDateBits;       //数据位
+                    sp1.StopBits = StopBits.One;    //停止位
+                    sp1.Parity = Parity.None;       //校验位
+                    
+                    if (sp1.IsOpen == true)//如果打开状态，则先关闭一下
+                    {
+                        sp1.Close();
+                    }
+
+                    //设置必要控件不可用
+                    cbSerial.Enabled = false;
+
+                    sp1.Open();     //打开串口
+                    btnSwitch.Text = "关闭串口";
+                }
+                catch (System.Exception ex)
+                {
+                    MessageBox.Show("Error:" + ex.Message, "Error");
+                    //tmSend.Enabled = false; Timer
+                    return;
+                }
+            }
+            else
+            {
+                //恢复控件功能
+                //设置必要控件不可用
+                cbSerial.Enabled = true;
+
+                sp1.Close();                    //关闭串口
+                btnSwitch.Text = "打开串口";
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (!sp1.IsOpen) //如果没打开
+            {
+                MessageBox.Show("请先打开串口！", "Error");
+                return;
+            }
+            if (pictureBox1.Image != null)
+            {
+                //定义一个Bitmap对象来存放图片
+                Bitmap bmp = new Bitmap(pictureBox1.Image);
+                //Bitmap bmp =(Bitmap)Image.FromFile(fileName);
+
+                Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+                //以可读写的方式锁定全部位图像素
+                BitmapData bmpData = bmp.LockBits(rect, ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);  //bmp.PixelFormat
+
+                //得到首地址
+                IntPtr ptr = bmpData.Scan0;
+                //位图字节数
+                int bytes = bmp.Width * bmp.Height * 3;
+                //定义位图数组
+                byte[] rgbValues = new byte[bytes];
+                //复制被锁定的位图像素值到该数组内
+                System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
+                //下载rgb数据
+                sp1.Write(rgbValues, 0, rgbValues.Length);
+            }
+        }
     }
 }
+
+
+
+ 
